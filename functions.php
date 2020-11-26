5c3279e8baedd4f2078765bf0c249c14098f3fc6
Mathias Eggert Marloth <dj-ert@hotmail.com>
2020-11-26 18:53:35 +0100

    structure

--- functions.php
+++ functions.php
@@ -2,7 +2,9 @@
 add_action( 'wp_enqueue_scripts', 'my_theme_enqueue_styles' );
 function my_theme_enqueue_styles() {
     wp_enqueue_style( 'child-style', get_stylesheet_uri(),
-        array( 'parenthandle' ), 
+        array( 'twentytweny' ), 
         wp_get_theme()->get('Version')
     );
+
+    wp_enqueue_style( 'my-style', get_template_directory_uri() . '/css/style.css', array());
 }
