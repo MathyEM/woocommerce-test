a55436e422f66a8665d546c9759824558e3b2963
Mathias Eggert Marloth <dj-ert@hotmail.com>
2020-12-10 15:03:12 +0100

    Added employee title and phone number

--- archive-employees.php
+++ archive-employees.php
@@ -28,10 +28,18 @@
             
                 <!-- the loop -->
                 <?php while ( $the_query->have_posts() ) : $the_query->the_post(); ?>
-                    <?php $department = wp_get_post_terms($post->ID, 'departments'); ?>
-
+                    <?php
+                        $department = wp_get_post_terms($post->ID, 'departments');
+                        $job_title = get_post_meta($post->ID, 'employee_title')[0];
+                        $tel_no = get_post_meta($post->ID, 'employee_tel')[0];
+                    
+                    ?>
+                    <?php the_post_thumbnail('thumbnail'); ?>
                     <h2><?php the_title(); ?></h2>
-                    <h3><?= $department[0]->name; ?></h3>
+                    <h3><?=$department[0]->name;?></h3>
+                    <h4><?=$job_title;?></h4>
+                    <a href="tel:0045<?=$tel_no?>"><h4>+45 <?=$tel_no;?></h4></a>
+                    <pre><?php var_dump($job_title) ?></pre>
                     <br>
                 <?php endwhile; ?>
                 <!-- end of the loop -->
