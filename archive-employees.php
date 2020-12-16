cd0ce29a33783bef222931600c886639221f6012
Mathias Eggert Marloth <dj-ert@hotmail.com>
2020-12-16 12:51:36 +0100

    Added base styling and sections for each department

--- archive-employees.php
+++ archive-employees.php
@@ -1,55 +1,59 @@
 <?php get_header(); ?>
     <div class="container">
         <h1>Hello World!</h1>
-        <?php
-            $terms = get_terms( array(
-                'taxonomy' => 'departments',
-                'hide_empty' => false,
-            ) );
+        <div class="employees-container container">
+            <?php
+                $terms = get_terms( array(
+                    'taxonomy' => 'departments',
+                    'hide_empty' => false,
+                ) );
+                
+                foreach ($terms as $term) {            
+                    // the query
+                    $args = array( 
+                        'tax_query' => array(
+                            array (
+                                'taxonomy' => 'departments',
+                                'field' => 'slug',
+                                'terms' => $term->slug,
+                            )
+                        ),
+                        'post_type' => 'employees',
+                    );
 
-            // the query
-            $args = array( 
-                'tax_query' => array(
-                    array (
-                        'taxonomy' => 'departments',
-                        'field' => 'slug',
-                        'terms' => 'salgsafdeling',
-                    )
-                ),
-                'post_type' => 'employees',
-            );
-
-            $the_query = new WP_Query( $args ); ?>
-
-            
-            <?php if ( $the_query->have_posts() ) : ?>
-            
-                <!-- pagination here -->
-            
-                <!-- the loop -->
-                <?php while ( $the_query->have_posts() ) : $the_query->the_post(); ?>
-                    <?php
-                        $department = wp_get_post_terms($post->ID, 'departments');
-                        $job_title = get_post_meta($post->ID, 'employee_title')[0];
-                        $tel_no = get_post_meta($post->ID, 'employee_tel')[0];
-                    
-                    ?>
-                    <?php the_post_thumbnail('thumbnail'); ?>
-                    <h2><?php the_title(); ?></h2>
-                    <h3><?=$department[0]->name;?></h3>
-                    <h4><?=$job_title;?></h4>
-                    <a href="tel:0045<?=$tel_no?>"><h4>+45 <?=$tel_no;?></h4></a>
-                    <pre><?php var_dump($job_title) ?></pre>
-                    <br>
-                <?php endwhile; ?>
-                <!-- end of the loop -->
-            
-                <!-- pagination here -->
-            
-                <?php wp_reset_postdata(); ?>
-            
-            <?php else : ?>
-                <p><?php _e( 'Sorry, no posts matched your criteria.' ); ?></p>
-            <?php endif; ?>
+                    $the_query = new WP_Query( $args );
+            ?>            
+                <?php if ( $the_query->have_posts() ) : ?>
+                    <!-- <pre>
+                        <?php //var_dump($terms); ?>
+                    </pre> -->
+                    <div class="row gy-5 gx-5">
+                        <!-- the loop -->
+                        <div class="department-card p-2 bg-success col-12 col-md-12 col-lg-6">
+                            <h2><?=$term->name?></h2>
+                        </div>
+                        <?php while ( $the_query->have_posts() ) : $the_query->the_post(); ?>
+                            <?php
+                                $department = wp_get_post_terms($post->ID, 'departments');
+                                $job_title = get_post_meta($post->ID, 'employee_title')[0];
+                                $tel_no = get_post_meta($post->ID, 'employee_tel')[0];
+                            
+                            ?>
+                            <div class="employee-container p-2 col-6 col-md-4 col-lg-2">
+                                <?php the_post_thumbnail('thumbnail'); ?>
+                                <h2><?php the_title(); ?></h2>
+                                <h3><?=$department[0]->name;?></h3>
+                                <h4><?=$job_title;?></h4>
+                                <a href="tel:0045<?=$tel_no?>"><h4>+45 <?=$tel_no;?></h4></a>
+                            </div>
+                        <?php endwhile; ?>
+                        <!-- end of the loop -->            
+                        <?php wp_reset_postdata(); ?>
+                    </div>
+                <?php else : ?>
+                    <p><?php _e( 'Sorry, no posts matched your criteria.' ); ?></p>
+                <?php endif; ?>
+            <?php } ?>
+        </div>
     </div>
 <?php get_footer(); ?>
--- functions.php
+++ functions.php
@@ -59,3 +59,114 @@ function create_departments_taxonomy() {
     'rewrite' => array( 'slug' => 'departments' ),
   ));
 }
+
+/*===================================================================================
+* Add global options
+* =================================================================================*/
+
+/**
+ *
+ * The page content surrounding the settings fields. Usually you use this to instruct non-techy people what to do.
+ *
+ */
+function theme_settings_page(){ 
+	?>
+	<div class="wrap">
+		<h1>Virksomhedsinformation</h1>
+		<p>Dette er globale virksomhedsoplysninger. Ændringer kan ses flere stedet på websitet</p>
+		<form method="post" action="options.php">
+			<?php
+			settings_fields("section");
+			do_settings_sections("theme-options");
+			submit_button();
+			?>
+		</form>
+	</div>
+	
+	<?php }
+
+/**
+ *
+ * Next comes the settings fields to display. Use anything from inputs and textareas, to checkboxes multi-selects.
+ *
+ */
+
+// Street address
+function display_business_name_element(){ ?>
+	
+	<input type="text" name="business_name" placeholder="Indtast virksomhedsnavn" value="<?php echo get_option('business_name'); ?>" size="35">
+
+<?php }
+
+// Phone
+function display_support_phone_element(){ ?>
+	
+	<input type="tel" name="support_phone" placeholder="Indtast telefonnummer" value="<?php echo get_option('support_phone'); ?>" size="35">
+
+<?php }
+
+// Street address
+function display_street_address_element(){ ?>
+	
+	<input type="text" name="street_address" placeholder="Indtast gade og husnummer" value="<?php echo get_option('street_address'); ?>" size="35">
+
+<?php }
+
+// City & zipcode
+function display_city_element(){ ?>
+	
+	<input type="text" name="city" placeholder="Indtast by og postnummer" value="<?php echo get_option('city'); ?>" size="35">
+
+<?php }
+
+// Email
+function display_support_email_element(){ ?>
+	
+	<input type="email" name="support_email" placeholder="Indtast email" value="<?php echo get_option('support_email'); ?>" size="35">
+	
+<?php }
+
+/**
+ *
+ * Here you tell WP what to enqueue into the <form> area. You need:
+ *
+ * 1. add_settings_section
+ * 2. add_settings_field
+ * 3. register_setting
+ *
+ */
+
+function display_custom_info_fields(){
+	
+	add_settings_section("section", "Virksomhedsinformation", null, "theme-options");
+
+    add_settings_field("business_name", "Virksomhedsnavn", "display_business_name_element", "theme-options", "section");
+	add_settings_field("support_phone", "Support Telefon", "display_support_phone_element", "theme-options", "section");
+	add_settings_field("support_email", "Support Email", "display_support_email_element", "theme-options", "section");
+	add_settings_field("street_address", "Gade", "display_street_address_element", "theme-options", "section");
+	add_settings_field("city", "By og postnummer", "display_city_element", "theme-options", "section");
+
+    register_setting("section", "business_name");
+	register_setting("section", "support_phone");
+	register_setting("section", "support_email");
+	register_setting("section", "street_address");
+	register_setting("section", "city");
+	
+}
+
+add_action("admin_init", "display_custom_info_fields");
+
+/**
+ *
+ * Tie it all together by adding the settings page to wherever you like. For this example it will appear
+ * in Settings > Contact Info
+ *
+ */
+
+function add_custom_info_menu_item(){
+	
+	add_options_page("Virksomhedsinformation", "Virksomhedsinformation", "manage_options", "contact-info", "theme_settings_page");
+	
+}
+
+add_action("admin_menu", "add_custom_info_menu_item");
