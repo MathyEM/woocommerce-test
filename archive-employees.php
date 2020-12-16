f576f8b71fb15045fac8a9a164dc570d6d5e80bd
Mathias Eggert Marloth <dj-ert@hotmail.com>
2020-12-16 15:08:02 +0100

    Show more employee info on click

--- archive-employees.php
+++ archive-employees.php
@@ -27,9 +27,9 @@
                     <!-- <pre>
                         <?php //var_dump($terms); ?>
                     </pre> -->
-                    <div class="department-section">
+                    <div class="department-section mb-5">
                         <!-- the loop -->
-                        <div class="department-card bg-success text-white p-5">
+                        <div class="department-card bg-success text-white p-4">
                             <div class="row">
                                 <div class="pb-5 col-12">
                                     <h2><?=$term->name;?></h2>
@@ -51,12 +51,19 @@
                                 $tel_no = get_post_meta($post->ID, 'employee_tel')[0];
                             
                             ?>
-                            <div class="employee-container">
+                            <div class="employee-container position-relative">
                                 <?php the_post_thumbnail('medium'); ?>
-                                <h2><?php the_title(); ?></h2>
-                                <h3><?=$department[0]->name;?></h3>
-                                <h4><?=$job_title;?></h4>
-                                <a href="tel:0045<?=$tel_no?>"><h4>+45 <?=$tel_no;?></h4></a>
+                                <div class="employee-info position-absolute text-white w-100 h-100">
+                                    <input class="invisible position-absolute" type="checkbox" name="toggle-info-<?=$post->ID;?>" id="toggle-info-<?=$post->ID;?>">
+                                    <div class="info-overlay w-100 h-100 p-2">
+                                        <div class="info-wrapper float-left">
+                                            <h5><?php the_title(); ?></h5>
+                                            <h5 class="m-0"><?=$department[0]->name;?>, <?=$job_title;?></h5>
+                                            <a class="text-success m-0 mt-2" href="tel:0045<?=$tel_no?>"><h5>+45 <?=$tel_no;?></h5></a>
+                                        </div>
+                                        <label class="font-weight-bold float-right m-0 px-2" for="toggle-info-<?=$post->ID;?>"></label>
+                                    </div>
+                                </div>
                             </div>
                         <?php endwhile; ?>
                         <!-- end of the loop -->            
