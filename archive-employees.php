28088cad1edf56411649e4b6cf0f09554ffdcb2c
Mathias Eggert Marloth <dj-ert@hotmail.com>
2020-12-17 16:09:11 +0100

    Added bootstrap icons. Changed employee-info icons. Fixed grid positioning mistake due to oversized department header height

--- archive-employees.php
+++ archive-employees.php
@@ -51,7 +51,7 @@
                         <!-- the loop -->
                         <div class="department-card bg-success text-white p-4">
                             <div class="row">
-                                <div class="pb-5 col-12">
+                                <div class="pb-4 col-12">
                                     <h2><?=$term->name;?></h2>
                                 </div>
                                 <div class="col-6">
@@ -76,12 +76,12 @@
                                 <div class="employee-info position-absolute text-white w-100 h-100">
                                     <input class="invisible position-absolute" type="checkbox" name="toggle-info-<?=$post->ID;?>" id="toggle-info-<?=$post->ID;?>">
                                     <div class="info-overlay w-100 h-100 p-2">
-                                        <div class="info-wrapper float-left">
-                                            <h5><?php the_title(); ?></h5>
+                                        <div class="info-wrapper">
+                                            <h5 class="font-weight-bold"><?php the_title(); ?></h5>
                                             <h5 class="m-0"><?=$department[0]->name;?>, <?=$job_title;?></h5>
                                             <a class="text-success m-0 mt-2" href="tel:0045<?=$tel_no?>"><h5>+45 <?=$tel_no;?></h5></a>
                                         </div>
-                                        <label class="font-weight-bold float-right m-0 px-2" for="toggle-info-<?=$post->ID;?>"></label>
+                                        <label class="font-weight-bold m-0 px-2" for="toggle-info-<?=$post->ID;?>"><i class="bi bi-chevron-down"></i></label>
                                     </div>
                                 </div>
                             </div>
